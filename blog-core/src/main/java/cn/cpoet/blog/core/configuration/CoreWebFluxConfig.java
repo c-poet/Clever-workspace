a34f88d6eb4e38478956fceeb650b6cabff9eeed
CPoet <llzero54@foxmail.com>
2023-04-12 17:21:20 +0800

    feat: 登录

--- blog-core/src/main/java/cn/cpoet/blog/core/configuration/CoreWebFluxConfig.java
+++ blog-core/src/main/java/cn/cpoet/blog/core/configuration/CoreWebFluxConfig.java
@@ -1,7 +1,6 @@
 package cn.cpoet.blog.core.configuration;
 
 import cn.cpoet.blog.core.resolver.AppContextArgumentResolver;
-import cn.cpoet.blog.core.resolver.RequestContextArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
@@ -14,6 +13,5 @@ public class CoreWebFluxConfig implements WebFluxConfigurer {
     @Override
     public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
         configurer.addCustomResolver(new AppContextArgumentResolver());
-        configurer.addCustomResolver(new RequestContextArgumentResolver());
     }
 }
